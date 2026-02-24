9d454e11b19246bc7f77b8e7d1a815c2bd9f475a
Nathan Humeau <nathumea79@gmail.com>
2026-02-24 14:28:29 +0100

    fix(swagger): Add cdn for swagger-ui-express + delete all unused functions

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ const app = express();
 // Parse incoming JSON requests
 app.use(express.json());
 
+const swaggerUiOptions = {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+  customJs: [
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.min.js'
+  ]
+};
+
 // API Documentation Endpoint
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, swaggerUiOptions));
 
 // Root endpoint to verify API is running
 app.get("/", (_req, res) => {
@@ -26,20 +34,6 @@ app.use("/todos", todoRouter);
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
 });
-
-function unusedHelper() {
-  var x = 42;
-  var tmp = x * 2;
-  return tmp;
-}
-
-function anotherDeadFunction(data) {
-  var result = [];
-  for (var i = 0; i < data.length; i++) {
-    result.push(data[i]);
-  }
-  return result;
-}
 
 const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== "test") {
